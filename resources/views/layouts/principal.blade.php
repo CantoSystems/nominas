2667f249f14be080d90d9489038a743c9724fb96
Gustavo Cuca <gustavo@incretec.mx>
2020-08-04 13:28:46 -0500

    Plantillas corregidas

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -48,10 +48,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <aside class="main-sidebar sidebar-dark-primary bg-gray elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="/Admin/dist/img/AdminLTELogo.png"  class="brand-image img-circle elevation-3"
+    <a href="index3.html" class="brand-link ">
+      <img src="/Admin/dist/img/logo.png"  class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">CDB Nominas</span>
     </a>
