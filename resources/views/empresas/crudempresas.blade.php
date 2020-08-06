49ab938864e8e85f9942153cce88e823e2b974e4
unknown <proyectosti@incretec.mx>
2020-08-06 10:29:36 -0500

    iconos

--- resources/views/empresas/crudempresas.blade.php
+++ resources/views/empresas/crudempresas.blade.php
@@ -5,7 +5,8 @@
         <h3 class="card-title">Empresas</h3>
     </div>
     <div class="card-body">
-    <form action="{{ route('acciones')}}" method="GET">
+    <form action="{{ route('acciones')}}" method="POST">
+    @csrf
     		<div class="row">
     			<div class="col-sm-6">
                     <div class="form-group">
@@ -132,24 +133,13 @@
                         <div class="margin">
                             <div class="btn-group">
                                 <div class="form-group">
-                                    <button type="button" class="btn btn-block btn-secondary">
-                                        <svg width="3em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M9 1H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h5v-1H4a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h5v2.5A1.5 1.5 0 0 0 10.5 6H13v2h1V6L9 1z"/>
-                                            <path fill-rule="evenodd" d="M13.5 10a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"/>
-                                            <path fill-rule="evenodd" d="M13 12.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0v-2z"/>
-                                        </svg>
-                                    </button>
+                                    <button type="button" class="btn btn-block btn-secondary"><i class="fas fa-user-plus"></i></button>
                                 </div>
                                 <div class="form-group">
-                                    <button type="button" class="btn btn-block btn-secondary" style='width:70px; height:40px'><i class="fas fa-pen-square"></i></button>
+                                    <button type="button" class="btn btn-block btn-secondary"><i class="fas fa-pen-square"></i></button>
                                 </div>
                                 <div class="form-group">
-                                    <button type="button" class="btn btn-block btn-secondary">
-                                        <svg width="3em" height="1em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
-                                            <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
-                                        </svg>
-                                    </button>
+                                    <button type="button" class="btn btn-block btn-secondary"><i class="fas fa-trash-alt"></i></button>
                                 </div>
                             </div>
                         </div>
