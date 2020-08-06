0b19296a90e9a881f4ef49ac75b9ce1bc665851a
unknown <proyectosti@incretec.mx>
2020-08-06 00:38:13 -0500

    correcciones a Crud Empresas

--- resources/views/empresas/crudempresas.blade.php
+++ resources/views/empresas/crudempresas.blade.php
@@ -13,67 +13,67 @@
                         <input type="text" name="nombre" class="form-control" placeholder="Nombre">
                     </div>
                 </div>    
-                <div class="col-sm-6">
+                <div class="col-sm-1">
                     <div class="form-group">
                         <label>Clave:</label>
                         <input type="text" name="clave" class="form-control" placeholder="Clave">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-5">
                     <div class="form-group">
                         <label>RFC (Empresa):</label>
                         <input type="text" name="rfc" min="13" class="form-control" placeholder="RFC (Empresa):">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-2">
                     <div class="form-group">
                         <label>Seguro social:</label>
                         <input type="text" name="segurosocial" max="11" class="form-control" placeholder="NSS:">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-2">
                     <div class="form-group">
                         <label>Registro estatal</label>
                         <input type="text" name="registro_estatal" class="form-control" placeholder="NSS:">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-4">
                     <div class="form-group">
                         <label>Calle:</label>
                         <input type="text" name="calle" class="form-control" placeholder="Calle:">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-2">
                     <div class="form-group">
                         <label>Número interno:</label>
                         <input type="text" name="num_interno" max="6000" min="1" class="form-control" placeholder="Número interno:">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-2">
                     <div class="form-group">
                         <label>Número externo:</label>
                         <input type="text" name="num_externo" class="form-control" placeholder="Número externo:">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-3">
                     <div class="form-group">
                         <label>Colonia:</label>
                         <input type="text" name="colonia" class="form-control" placeholder="Colonia:">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-3">
                     <div class="form-group">
                         <label>Municipio:</label>
                         <input type="text" name="municipio" class="form-control" placeholder="Municipio:">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-3">
                     <div class="form-group">
                         <label>Ciudad:</label>
                         <input type="text" name="ciudad" class="form-control" placeholder="Ciudad:">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-3">
                     <div class="form-group">
                         <label>País:</label>
                         <input type="text" name="pais" class="form-control" placeholder="País:">
@@ -85,19 +85,19 @@
                         <input type="text" name="representante_legal" max="13" class="form-control" placeholder="Representante legal:">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-3">
                     <div class="form-group">
                         <label>RFC (Representante legal):</label>
                         <input type="text" name="rfc_representante" class="form-control" placeholder="RFC (Representante legal):">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-3">
                     <div class="form-group">
                         <label>Teléfono:</label>
                         <input type="text" name="telefono"  max="10" class="form-control" placeholder="Teléfono:">
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-12">
                     <div class="form-group">
                         <label>Correo electrónico:</label>
                         <input type="email" name="email" class="form-control" placeholder="Correo electrónico:">
@@ -110,28 +110,16 @@
                         <div class="margin">
                             <div class="btn-group">
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-block btn-secondary" name="acciones" value="primero" id="primero">
-                                            <<
-                                    </button>
+                                    <button type="submit" class="btn btn-block btn-secondary" name="acciones" value="primero" id="primero"><i class="fas fa-backward"></i></button>
                                 </div>
                                 <div class="form-group">
-                                <button type="submit" class="btn btn-block btn-secondary" name="acciones" value="atras" id="atras">
-                                    <svg width="3em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.354 10.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L6.207 7.5H11a.5.5 0 0 1 0 1H6.207l2.147 2.146z"/>
-                                    </svg>
-                                </button>
+                                <button type="submit" class="btn btn-block btn-secondary" name="acciones" value="atras" id="atras"><i class="fas fa-arrow-circle-left"></i></button>
                                 </div>
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-block btn-secondary" name="acciones" value="siguiente" id="siguiente">
-                                        <svg width="3em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm5.646 10.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z"/>
-                                        </svg>
-                                    </button>
+                                    <button type="submit" class="btn btn-block btn-secondary" name="acciones" value="siguiente" id="siguiente"><i class="fas fa-arrow-circle-right"></i></button>
                                 </div>
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-block btn-secondary" name="acciones" value="ultimo" id="ultimo">
-                                            >>
-                                    </button>
+                                    <button type="submit" class="btn btn-block btn-secondary" name="acciones" value="ultimo" id="ultimo"><i class="fas fa-forward"></i></button>
                                 </div>
                             </div>
                         </div>
@@ -153,12 +141,7 @@
                                     </button>
                                 </div>
                                 <div class="form-group">
-                                    <button type="button" class="btn btn-block btn-secondary">
-                                        <svg width="3em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                        </svg>
-                                    </button>
+                                    <button type="button" class="btn btn-block btn-secondary" style='width:70px; height:40px'><i class="fas fa-pen-square"></i></button>
                                 </div>
                                 <div class="form-group">
                                     <button type="button" class="btn btn-block btn-secondary">
