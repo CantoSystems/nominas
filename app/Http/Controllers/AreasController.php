cd8d1daf5f277b7a8059b2fde314372bd39ff675
Gustavo Cuca <gustavo@incretec.mx>
2020-08-20 16:08:35 -0500

    Areas Crud

---
+++ app/Http/Controllers/AreasController.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Session;
+use DB;
+use Illuminate\Support\Facades\Schema;
+
+class AreasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $clv=Session::get('clave_empresa');
+        $clv_empresa=$this->conectar($clv);
+
+        \Config::set('database.connections.DB_Serverr', $clv_empresa);
+        $accion= $request->acciones;
+        $clv=$request->identificador;
+           switch ($accion) {
+               case '':
+                $areas = DB::connection('DB_Serverr')->select('select * from Areas');
+                $cont=count($areas);
+                $aux=$areas[0];
+                return view('Areas.area',compact('aux'));
+                   break;
+               case 'atras':
+                $areas = DB::connection('DB_Serverr')->select('select * from Areas where id > :id',['id' => $clv]);
+                    if($areas==""){
+                        $areas = DB::connection('DB_Serverr')->select('select * from Areas');
+                        $cont=count($areas);
+                        $aux=$areas[$cont-1];
+                        return view('Areas.area',compact('aux')); 
+                    }
+                    $aux=$areas[0];
+                    return view('Areas.area',compact('aux'));                
+                break;
+               case 'siguiente':
+                   $emp= Empresa::where('clave',$clv)->first();
+                   $indic= $emp->id;
+                   $empresa= Empresa::where('id','>',$indic)->first();
+                   if($empresa==""){
+                      $empresa= Empresa::first();  
+                   }
+                   return view('empresas.crudempresas', compact('empresa'));
+               break;
+               case 'primero':
+                   $empresa= Empresa::first();
+                   return view('empresas.crudempresas', compact('empresa'));
+               break;
+               case 'ultimo':
+                   $empresa= Empresa::get()->last(); 
+                   return view('empresas.crudempresas', compact('empresa')); 
+               break;
+               case 'registrar':
+               $this->registrar($request);
+               $empresa= Empresa::first();
+               return view('empresas.crudempresas', compact('empresa'));
+               break;
+               case 'actualizar':
+                   $this->actualizar($request);
+                   $empresa= Empresa::first();
+                   return view('empresas.crudempresas', compact('empresa'));
+               break;
+               default:
+                   # code...
+                   break;
+}
+        
+
+        
+     
+    }
+
+
+public function conectar($clv)
+{
+
+    $configDb = [
+        'driver'      => 'mysql',
+        'host'        => env('DB_HOST', 'localhost'),
+        'port'        => env('DB_PORT', '3306'),
+        'database'    => $clv,
+        'username'    => env('DB_USERNAME', 'javier'),
+        'password'    => env('DB_PASSWORD', 'tnvsi2182019'),
+        'unix_socket' => env('DB_SOCKET', ''),
+        'charset'     => 'utf8',
+        'collation'   => 'utf8_unicode_ci',
+        'prefix'      => '',
+        'strict'      => true,
+        'engine'      => null,
+];
+
+return $configDb;
+
+}
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/Areas/area.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.principal')
+@section('content')
+<div class="card card-secondary">
+	<div class="card-header">
+        <h3 class="card-title">Areas</h3>
+    </div>
+    <div class="card-body">
+    <form action="{{ route('areas.index')}}" method="GET">
+    		<div class="row">
+    			<div class="col-sm-12">
+                    <div class="form-group">
+                        <label>Nombre:</label>
+                    <input type="text" name="nombre" class="form-control" value="{{$aux->area}}"  onkeyup="mayus(this);">
+                    <input type="hidden" name="identificador" class="form-control" value="{{$aux->id}}"  onkeyup="mayus(this);">
+
+                    </div>
+                </div>
+                
+                <div class="col-sm-5">
+                    <div class="card-body">
+                        <div class="margin">
+                            <div class="btn-group">
+                                <div class="form-group">
+                                    <button type="submit"  name="acciones" value="primero" id="primero" style='width:70px; height:40px'><i class="fas fa-backward" ></i></button>
+                                </div>
+                                <div class="form-group">
+                                <button type="submit" name="acciones" value="atras" id="atras" style='width:70px; height:40px'><i class="fas fa-arrow-circle-left"></i></button>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" name="acciones" value="siguiente" id="siguiente" style='width:70px; height:40px'><i class="fas fa-arrow-circle-right"></i></button>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" name="acciones" value="ultimo" id="ultimo" style='width:70px; height:40px'><i class="fas fa-forward"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-sm-5">
+                    <div class="card-body">
+                        <div class="margin">
+                            <div class="btn-group">
+                                <div class="form-group">
+                                    <button type="button" id="nuevo" style='width:70px; height:40px'> <i class="fas fa-user-plus"></i></button>
+                                </div>
+                                <div class="form-group">
+                                 <button type="button" id="actualizar" style='width:70px; height:40px'> <i class="fas fa-pen-square"></i></button>
+                                </div>
+                                <div class="form-group">
+                                    <a href="#" id="eliminar" data-target="#modal-deletenom- data-toggle="modal" style='width:70px; height:40px'>
+                                        <button type="button" style='width:70px; height:40px'>
+                                            <i class="far fa-trash-alt">
+                                            </i>
+                                        </button></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="col-sm-2">
+                    <div class="card-body">
+                        <div class="margin">
+                            <div class="btn-group">
+                                <div class="form-group">
+                                    
+                                       <button class="btn btn-primary" id="nuevo_reg" name="acciones" value="registrar" type="submit" style="display: none;width:70px; height:40px'"><i class="fas fa-save"></i></button>
+                                        
+                                    
+                                </div>
+                                <div class="form-group">
+                                    <button class="btn btn-warning" name="acciones" value="actualizar" id="actualizar_reg" type="submit" style="display: none;width:70px; height:40px'"><i class="fas fa-save"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>	
+    	</form>	
+        
+   	</div> 	
+</div>	
+@endsection
+
+
+
+
+                    
