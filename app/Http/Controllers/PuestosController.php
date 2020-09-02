34f62ca13e3e197a86f066d7b06b6adf90395f6c
proyectosti@incretec.mx <proyectosti@incretec.mx>
2020-09-02 02:09:59 -0500

    Correccion en botones

--- app/Http/Controllers/PuestosController.php
+++ app/Http/Controllers/PuestosController.php
@@ -32,7 +32,8 @@ class PuestosController extends Controller
                 if($aux==""){
                     $aux = DB::connection('DB_Serverr')->table('puestos')->get()->last();
                 }
-                return view('puetos.puestos',compact('aux'));
+                $puestos= DB::connection('DB_Serverr')->table('puestos')->get();
+                return view('puestos.puestos',compact('aux','puestos'));
                  break;
 
 
@@ -41,27 +42,32 @@ class PuestosController extends Controller
                 if($aux==""){
                     $aux = DB::connection('DB_Serverr')->table('puestos')->get()->first();
                 }
-                return view('puestos.puestos',compact('aux'));
+                $puestos= DB::connection('DB_Serverr')->table('puestos')->get();
+                return view('puestos.puestos',compact('aux','puestos'));
                break;
                case 'primero':
                 $aux = DB::connection('DB_Serverr')->table('puestos')->first();
-                return view('puestos.puestos',compact('aux'));
+                $puestos= DB::connection('DB_Serverr')->table('puestos')->get();
+                return view('puestos.puestos',compact('aux','puestos'));
                break;
                case 'ultimo':
                 $aux = DB::connection('DB_Serverr')->table('puestos')->get()->last();
-                return view('puestos.puestos',compact('aux')); 
+                $puestos= DB::connection('DB_Serverr')->table('puestos')->get();
+                return view('puestos.puestos',compact('aux','puestos')); 
                break;
                case 'registrar':
                $this->registrar($request);
                $aux = DB::connection('DB_Serverr')->table('puestos')->get()->first();
-                return view('puestos.puestos',compact('aux'));
+               $puestos= DB::connection('DB_Serverr')->table('puestos')->get();
+               return view('puestos.puestos',compact('aux','puestos'));
                break;
                case 'actualizar':
                 $aux1 = DB::connection('DB_Serverr')->table('puestos')->where('clave_puesto',$clave_p)->first();
                    if($aux1!==""){
-                   DB::connection('DB_Serverr')->table('puestos')->where('clave_puesto',$request->clave_puesto)->update(['area'=>$request->clave_puesto]);
+                   DB::connection('DB_Serverr')->table('puestos')->where('clave_puesto',$request->clave_puesto)->update(['nombre_puesto'=>$request->puesto]);
                    $aux = DB::connection('DB_Serverr')->table('puestos')->get()->first();
-                   return view('puestos.puestos',compact('aux'));
+                   $puestos= DB::connection('DB_Serverr')->table('puestos')->get();
+                   return view('puestos.puestos',compact('aux','puestos'));
                    }
                break;
                case 'eliminar':
@@ -80,6 +86,7 @@ class PuestosController extends Controller
 
 public function registrar($datos){
     $clv=Session::get('clave_empresa');
+    $clave_puesto= $this->generador();
     $configDb = [
         'driver'      => 'mysql',
         'host'        => env('DB_HOST', 'localhost'),
@@ -96,10 +103,20 @@ public function registrar($datos){
 ];
 
     \Config::set('database.connections.DB_Serverr', $configDb);
-    DB::connection('DB_Serverr')->insert('insert into puestos (clave_puesto, puesto)
-    values (?,?)',[$datos->clave_puesto,$datos->nombre_puesto]);
+    DB::connection('DB_Serverr')->insert('insert into puestos (clave_puesto, nombre_puesto)
+    values (?,?)',[$clave_puesto,$datos->puesto]);
 }
 
+public function generador(){
+	$raiz= '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$codigo='';
+	for ($i=0; $i < 3; $i++) { 
+		$letra= $raiz[mt_rand(0, 4 - 1)];
+		$codigo .=$letra;
+	}
+	return $codigo;
+    }
+    
 public function conectar($clv)
 {
 
