fa8c9c93172f2b0accc904fd315adc20fdb74654
Gustavo Cuca <gustavo@incretec.mx>
2020-09-02 12:04:11 -0500

    validaciones

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -184,5 +184,24 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <script>
     $('.telefono').mask('(00) 0000-0000');
 </script>
+
+<script type="text/javascript">
+  function validar(e) { 
+  tecla = (document.all) ? e.keyCode : e.which; 
+  if (tecla==8) return true; 
+  patron =/[A-Za-z\s]/; 
+  te = String.fromCharCode(tecla); 
+  return patron.test(te); 
+  }
+</script>
+
+<script type="text/javascript">
+function numeros(event) { 
+if(event.charCode >=48 && event.charCode <=57 ){
+  return true;
+}
+return false;
+}
+</script>
 </body>
 </html>
