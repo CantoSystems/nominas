be950d62c0de54b6e5aba84862085b631e02102d
proyectosti@incretec.mx <proyectosti@incretec.mx>
2020-09-06 23:14:30 -0500

    catalogo de bancos terminado

--- app/Http/Controllers/ConceptosController.php
+++ app/Http/Controllers/ConceptosController.php
@@ -36,7 +36,15 @@ class ConceptosController extends Controller
         break;
 
         case 'atras':
-          
+            $id=DB::connection('DB_Serverr')->table('conceptos')->
+            where('clave_concepto',$request->clave_concepto)->first();
+               $aux=DB::connection('DB_Serverr')->table('conceptos')->where('id','<',$id->id)
+            ->orderBy('id','desc')
+            ->first();
+            if(is_null($aux)){
+                $aux=DB::connection('DB_Serverr')->table('conceptos')->get()->last();
+            }
+            return view('conceptos.conceptos', compact('aux'));
         break;
 
         case 'siguiente':
@@ -52,7 +60,9 @@ class ConceptosController extends Controller
         break;
 
         case 'registrar':
-          
+            $this->registar($request);
+            $aux = DB::connection('DB_Serverr')->table('conceptos')->first(); 
+            return view('conceptos.conceptos',compact('aux'));
         break;
 
         case 'actualizar':
@@ -70,4 +80,37 @@ class ConceptosController extends Controller
     }
    
  }
+ public function registrar($datos){
+    $clv=Session::get('clave_empresa');
+    $clave_concepto= $this->generador();
+    $configDb = [
+        'driver'      => 'mysql',
+        'host'        => env('DB_HOST', 'localhost'),
+        'port'        => env('DB_PORT', '3306'),
+        'database'    => $clv,
+        'username'    => env('DB_USERNAME', 'root'),
+        'password'    => env('DB_PASSWORD', ''),
+        'unix_socket' => env('DB_SOCKET', ''),
+        'charset'     => 'utf8',
+        'collation'   => 'utf8_unicode_ci',
+        'prefix'      => '',
+        'strict'      => true,
+        'engine'      => null,
+];
+
+    \Config::set('database.connections.DB_Serverr', $configDb);
+    DB::connection('DB_Serverr')->insert('insert into conceptos (clave_concepto,concepto,formula,naturaleza,manejo
+    ,cantidad,importe,monto,ispt,imss,infonavit,estatal)
+    values (?,?,?,?,?,?,?,?,?,?,?,?)',[$clave_concepto,$datos->concepto,$datos->formula,$datos->naturaleza
+    ,$datos->manejo,$datos->cantidad,$datos->importe,$datos->monto,$ispt,$imss,$infonavit,$estatal]);
+}
+public function generador(){
+	$raiz= '0123456789';
+	$codigo='';
+	for ($i=0; $i < 3; $i++) { 
+		$letra= $raiz[mt_rand(0, 4 - 1)];
+		$codigo .=$letra;
+	}
+	return $codigo;
+	}
 }
