314c6ece0a125dc441b8a990ac25e01e9275ac91
elizabeth <anely_lyzzye@hotmail.com>
2020-09-10 11:03:40 +0200

    conceptos terminada

--- app/Http/Controllers/ConceptosController.php
+++ app/Http/Controllers/ConceptosController.php
@@ -206,4 +206,28 @@ public function actualizar($datos){
      }
     }
 
+    public function eliminaconcepto($id){
+        $empresa= Session::get('clave_empresa');
+        $configDb = [
+        'driver'      => 'mysql',
+        'host'        => env('DB_HOST', 'localhost'),
+        'port'        => env('DB_PORT', '3306'),
+        'database'    => $empresa,
+        'username'    => env('DB_USERNAME', 'root'),
+        'password'    => env('DB_PASSWORD', ''),
+        'unix_socket' => env('DB_SOCKET', ''),
+        'charset'     => 'utf8',
+        'collation'   => 'utf8_unicode_ci',
+        'prefix'      => '',
+        'strict'      => true,
+        'engine'      => null,
+    ];
+    
+    \Config::set('database.connections.DB_Serverr', $configDb);
+    $aux1 = DB::connection('DB_Serverr')->table('conceptos')->where('id',$id)->delete();
+    $aux = DB::connection('DB_Serverr')->table('conceptos')->first(); 
+            return view('conceptos.conceptos',compact('aux'));
+
+    }
+
 }
