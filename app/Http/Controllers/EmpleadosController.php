8ed50ff9a44638f1f41a979564f365aa980bcc04
proyectosti@incretec.mx <proyectosti@incretec.mx>
2020-09-10 23:25:23 -0500

    adelanto de empleados

--- app/Http/Controllers/EmpleadosController.php
+++ app/Http/Controllers/EmpleadosController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Session;
+use DB;
 class EmpleadosController extends Controller
 {
-    //
+    public function conectar($clv)
+{
+
+    $configDb = [
+        'driver'      => 'mysql',
+        'host'        => env('DB_HOST', 'localhost'),
+        'port'        => env('DB_PORT', '3306'),
+        'database'    => $clv,
+        'username'    => env('DB_USERNAME', 'root'),
+        'password'    => env('DB_PASSWORD', ''),
+        'unix_socket' => env('DB_SOCKET', ''),
+        'charset'     => 'utf8',
+        'collation'   => 'utf8_unicode_ci',
+        'prefix'      => '',
+        'strict'      => true,
+        'engine'      => null,
+];
+
+return $configDb;
+
+}
+    public function index(Request $request){
+    	$clv=Session::get('clave_empresa');
+        $clv_empresa=$this->conectar($clv);
+
+        \Config::set('database.connections.DB_Serverr', $clv_empresa);
+        $accion= $request->acciones;
+    switch ($accion) {
+        case '':
+            return view('empleados.empleados');
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+    }
 }
