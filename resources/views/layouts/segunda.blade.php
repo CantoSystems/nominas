6b86d7fb5213de033fd18a8b60aca6edf2eec935
elizabeth <anely_lyzzye@hotmail.com>
2020-09-18 20:12:05 +0200

    pestania referencias mod empleados

--- resources/views/layouts/segunda.blade.php
+++ resources/views/layouts/segunda.blade.php
@@ -80,7 +80,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="{{ route('empleados.index')}}" class="nav-link active">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Empleados</p>
                 </a>
