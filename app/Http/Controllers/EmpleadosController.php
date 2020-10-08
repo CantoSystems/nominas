382e42a9605dff5fb5560ae5452b8bf76e462f76
Gustavo Cuca <gustavo@incretec.mx>
2020-10-08 18:35:57 -0500

    Modificacion Empleados Controller

--- app/Http/Controllers/EmpleadosController.php
+++ app/Http/Controllers/EmpleadosController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
-
-use Illuminate\Http\Request;
 use App\Banco;
-use Session;
+use Illuminate\Http\Request;
 use DB;
+use Illuminate\Support\Facades\Schema;
+use Session;
+
+
+
 class EmpleadosController extends Controller
 {
     public function conectar($clv)
@@ -41,12 +44,8 @@ return $configDb;
             $empleados=DB::connection('DB_Serverr')->table('empleados')->get();
             $departamentos=DB::connection('DB_Serverr')->table('departamentos')->get();
             $puestos=DB::connection('DB_Serverr')->table('puestos')->get();
-
-            $bancos= Banco::all();
-            //$bancos=DB::connection('DB_Serverr')->table('bancos')->get();
-
             $bancos=Banco::all();
-
+    
             return view('empleados.empleados',compact('empleados','departamentos','puestos','bancos'));
             break;
         case 'registrar':
@@ -98,10 +97,7 @@ return $configDb;
 
     public function registrar_empleado($datos)
     {
-        $datos->fecha_baja->validate([
-            'fecha_baja' => 'after:fecha_alta'
-    
-        ]);
+        
         
     $clv=Session::get('clave_empresa');
     $clv_empleado= $this->generador($datos->rfc);
@@ -121,14 +117,14 @@ return $configDb;
     nombre_madre, finado_padre, finado_madre, direccion_padre, direccion_madre, ocupacion_padre, ocupacion_madre, hijos, idiomas,
     funciones_oficina, maquinas_oficina, software, otras_funciones, beneficiario, beneficiario1, beneficiario2, beneficiario3,
     beneficiario4, parentesco, parentesco1, parentesco2, parentesco3, parentesco4, porcentaje, porcentaje1, porcentaje2, porcentaje3,
-    porcentaje4, primaria, inicio_primaria, fin_primaria, titulo_primaria, secundaria, inicio_secundaria, fin_secundaria, titulo_secundaria,
-    preparatoria, inicio_preparatoria, fin_preparatoria, titulo_preparatoria, profesional, inicio_profesional, fin_profesional,
-    titulo_profesional, otras, inicio_otras, fin_otras, titulo_otras, estudio_actual, carrera, grado, horario, vinvulacion_trabajo,
-    desvinculacion_trabajo, nombre_compania, direccion_compania, telefono_compania, sueldo, motivo_separacion, nombre_jefe, puesto_jefe,
-    solicitar_informes, razones, vinvulacion_trabajo1, desvinculacion_trabajo1, nombre_compania1, direccion_compania1, telefono_compania1, sueldo1, 
-    motivo_separacion1, nombre_jefe1, puesto_jefe1, solicitar_informes1, razones1, vinvulacion_trabajo2, desvinculacion_trabajo2, nombre_compania2, 
+    porcentaje4, primaria, duracion_primaria, titulo_primaria, secundaria, duracion_secundaria, titulo_secundaria,
+    preparatoria, duracion_preparatoria, titulo_preparatoria, profesional, duracion_profesional,
+    titulo_profesional, otras, duracion_otras, titulo_otras, estudio_actual, carrera, grado, horario, duracion_trabajo,
+     nombre_compania, direccion_compania, telefono_compania, sueldo, motivo_separacion, nombre_jefe, puesto_jefe,
+    solicitar_informes, razones, duracion_trabajo1, nombre_compania1, direccion_compania1, telefono_compania1, sueldo1, 
+    motivo_separacion1, nombre_jefe1, puesto_jefe1, solicitar_informes1, razones1, duracion_trabajo2, nombre_compania2, 
     direccion_compania2, telefono_compania2, sueldo2, motivo_separacion2, nombre_jefe2, puesto_jefe2, solicitar_informes2, razones2,
-    vinvulacion_trabajo3, desvinculacion_trabajo3, nombre_compania3, direccion_compania3, telefono_compania3, sueldo3, motivo_separacion3, nombre_jefe3, puesto_jefe3, 
+    duracion_trabajo3, nombre_compania3, direccion_compania3, telefono_compania3, sueldo3, motivo_separacion3, nombre_jefe3, puesto_jefe3, 
     solicitar_informes3, razones3, referencia, direccion_trabajo, telefono_referencia, ocupacion, tiempo, referencia1, direccion_trabajo1, telefono_referencia1, 
     ocupacion1, tiempo1, referencia2, direccion_trabajo2, telefono_referencia2, ocupacion2, tiempo2, tipo_trabajador, turno, contrato,
     contrato_documento, vigencia, horario_trabajoinicio,horario_trabajofin, sueldo_diario, nivel, categoria, tipo_sueldo, tipo_jornada, dias, horas_diarias,
@@ -143,13 +139,13 @@ return $configDb;
     $datos->lugar, $datos->umf, $datos->nombre_padre, $datos->nombre_madre, $datos->finado_padre, $datos->finado_madre, $datos->direccion_padre, $datos->direccion_madre, $datos->ocupacion_padre,
     $datos->ocupacion_madre, $datos->hijos, $datos->idiomas, $datos->funciones_oficina, $datos->maquinas_oficina, $datos->software, $datos->otras_funciones, $datos->beneficiario, $datos->beneficiario1, $datos->beneficiario2,
     $datos->beneficiario3, $datos->beneficiario4, $datos->parentesco, $datos->parentesco1, $datos->parentesco2, $datos->parentesco3, $datos->parentesco4, $datos->porcentaje, $datos->porcentaje1,
-    $datos->porcentaje2, $datos->porcentaje3, $datos->porcentaje4, $datos->primaria, $datos->inicio_rpimaria, $datos->fin_primaria, $datos->titulo_primaria, $datos->secundaria, $datos->inicio_secundaria,
-    $datos->fin_secundaria, $datos->titulo_secundaria, $datos->preparatoria, $datos->inicio_preparatoria, $datos->fin_preparatoria, $datos->titulo_preparatoria, $datos->profesional, $datos->inicio_profesional,
-    $datos->fin_profesional, $datos->titulo_profesional, $datos->otras, $datos->inicio_otras, $datos->fin_otras, $datos->titulo_otras, $datos->estudio_actual, $datos->carrera, $datos->grado, $datos->horario,
-    $datos->vinculacion_trabajo,$datos->desvinculacion_trabajo,$datos->nombre_compania,$datos->direccion_compania,$datos->telefono_compania,$datos->sueldo,$datos->motivo_separacion,$datos->nombre_jefe,$datos->puesto_jefe,
-    $datos->solicitar_informes,$datos->razones,$datos->vinculacion_trabajo1,$datos->desvinculacion_trabajo1,$datos->nombre_compania1,$datos->direccion_compania1,$datos->telefono_compania1,$datos->sueldo1,$datos->motivo_separacion1,
-    $datos->nombre_jefe1,$datos->puesto_jefe1,$datos->solicitar_informes1,$datos->razones1,$datos->vinculacion_trabajo2,$datos->desvinculacion_trabajo2,$datos->nombre_compania2,$datos->direccion_compania2,
-    $datos->telefono_compania2,$datos->sueldo2,$datos->motivo_separacion2,$datos->nombre_jefe2,$datos->puesto_jefe2,$datos->solicitar_informes2,$datos->razones2,$datos->vinculacion_trabajo3,$datos->desvinculacion_trabajo3,
+    $datos->porcentaje2, $datos->porcentaje3, $datos->porcentaje4, $datos->primaria, $datos->duracion_pimaria, $datos->titulo_primaria, $datos->secundaria, $datos->duracion_secundaria,
+    $datos->titulo_secundaria, $datos->preparatoria, $datos->duracion_preparatoria, $datos->titulo_preparatoria, $datos->profesional, $datos->duracion_profesional,
+    $datos->titulo_profesional, $datos->otras, $datos->duracion_otras, $datos->titulo_otras, $datos->estudio_actual, $datos->carrera, $datos->grado, $datos->horario,
+    $datos->duracion_trabajo,$datos->nombre_compania,$datos->direccion_compania,$datos->telefono_compania,$datos->sueldo,$datos->motivo_separacion,$datos->nombre_jefe,$datos->puesto_jefe,
+    $datos->solicitar_informes,$datos->razones,$datos->duracion_trabajo1,$datos->nombre_compania1,$datos->direccion_compania1,$datos->telefono_compania1,$datos->sueldo1,$datos->motivo_separacion1,
+    $datos->nombre_jefe1,$datos->puesto_jefe1,$datos->solicitar_informes1,$datos->razones1,$datos->duracion_trabajo2,$datos->nombre_compania2,$datos->direccion_compania2,
+    $datos->telefono_compania2,$datos->sueldo2,$datos->motivo_separacion2,$datos->nombre_jefe2,$datos->puesto_jefe2,$datos->solicitar_informes2,$datos->razones2,$datos->duracion_trabajo3,
     $datos->nombre_compania3,$datos->direccion_compania3,$datos->telefono_compania3,$datos->sueldo3,$datos->motivo_seperacion3,$datos->nombre_jefe3,$datos->puesto_jefe3,
     $datos->solicitar_informes3,$datos->razones3,$datos->referencia,$datos->direccion_trabajo,$datos->telefono_referencia,$datos->ocupacion,$datos->tiempo,$datos->referencia1,$datos->direccion_trabajo1,$datos->telefono_referencia1,
     $datos->ocupacion1,$datos->tiempo1,$datos->referencias2,$datos->direccion_trabajo2,$datos->telefono_referencia2,$datos->ocupacion2,$datos->tiempo2,$datos->tipo_trabajador,$datos->turno,$datos->contrato,
@@ -186,8 +182,8 @@ return $configDb;
       if($aux1!=="")
         {
          DB::connection('DB_Serverr')->table('empleados')->where('clave_empleado',$datos->clave_empleado)
-        ->update(['clave_empleado'=>$datos->clave_empleado,'clasificacion'=>$datos->clasificacion,'nombre'=>$datos->nombre,'apellido_paterno'=>$datos->apellido_paterno,'apellido_materno'=>$datos->apellido_materno,'fecha_alta'=>$datos->fecha_alta,'fecha_baja'=>$datos->fecha_baja,'causa_baja'=>$datos->causa_baja,'clave_departamento'=>$datos->clave_departamento,'clave_puesto'=>$datos->clave_puesto,'rfc'=>$datos->rfc,'curp'=>$datos->curp,'imss'=>$datos->imss,'afore'=>$datos->afore,'ine'=>$datos->ine,'pasaporte'=>$datos->pasaporte,'cartilla'=>$datos->cartilla,'licencia'=>$datos->licencia,'documento_migratorio'=>$datos->documento_migratorio,'calle'=>$datos->calle,'numero_interno'=>$datos->numero_interno,'numero_externo'=>$datos->numero_externo,'colonia'=>$datos->colonia,'cp'=>$datos->colonia,'ciudad'=>$datos->ciudad,'municipio'=>$datos->municipio,'estado'=>$datos->estado,'telefono_empleado'=>$datos->telefono_empleado,'correo'=>$datos->correo,'sexo'=>$datos->sexo,'estado_civil'=>$datos->estado_civil,'nacionalidad'=>$datos->nacionalidad,'tipo_sangre'=>$datos->tipo_sangre,'alergias'=>$datos->alergias,'estatura'=>$datos->estatura,'peso'=>$datos->peso,'estado_salud'=>$datos->estado_salud,'enfermedad_cronica'=>$datos->enfermedad_cronica,'deporte'=>$datos->deporte,'pasatiempo'=>$datos->pasatiempo,'asosiacion'=>$datos->asosiacion,'objetivo_vida'=>$datos->objetivo_vida,'fecha_nacimiento'=>$datos->fecha_nacimiento,'lugar'=>$datos->fecha_nacimiento,'umf'=>$datos->umf,'nombre_padre'=>$datos->nombre_padre,'nombre_madre'=>$datos->nombre_madre,'finado_padre'=>$datos->finado_padre,'finado_madre'=>$datos->finado_madre,'direccion_padre'=>$datos->direccion_padre,'direccion_madre'=>$datos->direccion_madre,'ocupacion_padre'=>$datos->ocupacion_padre,'ocupacion_madre'=>$datos->ocupacion_madre,'hijos'=>$datos->hijos,'idiomas'=>$datos->idiomas,'funciones_oficina'=>$datos->funciones_oficina,'maquinas_oficina'=>$datos->maquinas_oficina,'software'=>$datos->software,'otras_funciones'=>$datos->otras_funciones,'beneficiario'=>$datos->beneficiario,'beneficiario1'=>$datos->beneficiario1,'beneficiario2'=>$datos->beneficiario2,'beneficiario3'=>$datos->beneficiario3,'beneficiario4'=>$datos->beneficiario4,'parentesco'=>$datos->beneficiario4,'parentesco1'=>$datos->parentesco1,'parentesco2'=>$datos->parentesco2,'parentesco3'=>$datos->parentesco3,'parentesco4'=>$datos->parentesco4,'porcentaje'=>$datos->porcentaje,'porcentaje1'=>$datos->porcentaje1,'porcentaje2'=>$datos->porcentaje2,'porcentaje3'=>$datos->porcentaje3,'porcentaje4'=>$datos->porcentaje4,'primaria'=>$datos->primaria,'inicio_primaria'=>$datos->inicio_primaria,'fin_primaria'=>$datos->fin_primaria,'titulo_primaria'=>$datos->titulo_primaria,'secundaria'=>$datos->secundaria,'inicio_secundaria', 'fin_secundaria'=>$datos->fin_secundaria,'titulo_secundaria'=>$datos->titulo_secundaria,'preparatoria'=>$datos->preparatoria,
-            'inicio_preparatoria'=>$datos->inicio_preparatoria,'fin_preparatoria'=>$datos->fin_preparatoria,'titulo_preparatoria'=>$datos->titulo_preparatoria,'profesional'=>$datos->profesional,'inicio_profesional'=>$datos->inicio_profesional,'fin_profesional'=>$datos->fin_profesional,'titulo_profesional'=>$datos->titulo_profesional,'otras'=>$datos->otras,'inicio_otras'=>$datos->inicio_otras,'fin_otras'=>$datos->fin_otras,'titulo_otras'=>$datos->titulo_otras,'estudio_actual'=>$datos->estudio_actual,'carrera'=>$datos->carrera,'grado'=>$datos->grado,'horario'=>$datos->horario,'vinvulacion_trabajo'=>$datos->vinvulacion_trabajo,
+        ->update(['clave_empleado'=>$datos->clave_empleado,'clasificacion'=>$datos->clasificacion,'nombre'=>$datos->nombre,'apellido_paterno'=>$datos->apellido_paterno,'apellido_materno'=>$datos->apellido_materno,'fecha_alta'=>$datos->fecha_alta,'fecha_baja'=>$datos->fecha_baja,'causa_baja'=>$datos->causa_baja,'clave_departamento'=>$datos->clave_departamento,'clave_puesto'=>$datos->clave_puesto,'rfc'=>$datos->rfc,'curp'=>$datos->curp,'imss'=>$datos->imss,'afore'=>$datos->afore,'ine'=>$datos->ine,'pasaporte'=>$datos->pasaporte,'cartilla'=>$datos->cartilla,'licencia'=>$datos->licencia,'documento_migratorio'=>$datos->documento_migratorio,'calle'=>$datos->calle,'numero_interno'=>$datos->numero_interno,'numero_externo'=>$datos->numero_externo,'colonia'=>$datos->colonia,'cp'=>$datos->colonia,'ciudad'=>$datos->ciudad,'municipio'=>$datos->municipio,'estado'=>$datos->estado,'telefono_empleado'=>$datos->telefono_empleado,'correo'=>$datos->correo,'sexo'=>$datos->sexo,'estado_civil'=>$datos->estado_civil,'nacionalidad'=>$datos->nacionalidad,'tipo_sangre'=>$datos->tipo_sangre,'alergias'=>$datos->alergias,'estatura'=>$datos->estatura,'peso'=>$datos->peso,'estado_salud'=>$datos->estado_salud,'enfermedad_cronica'=>$datos->enfermedad_cronica,'deporte'=>$datos->deporte,'pasatiempo'=>$datos->pasatiempo,'asosiacion'=>$datos->asosiacion,'objetivo_vida'=>$datos->objetivo_vida,'fecha_nacimiento'=>$datos->fecha_nacimiento,'lugar'=>$datos->fecha_nacimiento,'umf'=>$datos->umf,'nombre_padre'=>$datos->nombre_padre,'nombre_madre'=>$datos->nombre_madre,'finado_padre'=>$datos->finado_padre,'finado_madre'=>$datos->finado_madre,'direccion_padre'=>$datos->direccion_padre,'direccion_madre'=>$datos->direccion_madre,'ocupacion_padre'=>$datos->ocupacion_padre,'ocupacion_madre'=>$datos->ocupacion_madre,'hijos'=>$datos->hijos,'idiomas'=>$datos->idiomas,'funciones_oficina'=>$datos->funciones_oficina,'maquinas_oficina'=>$datos->maquinas_oficina,'software'=>$datos->software,'otras_funciones'=>$datos->otras_funciones,'beneficiario'=>$datos->beneficiario,'beneficiario1'=>$datos->beneficiario1,'beneficiario2'=>$datos->beneficiario2,'beneficiario3'=>$datos->beneficiario3,'beneficiario4'=>$datos->beneficiario4,'parentesco'=>$datos->beneficiario4,'parentesco1'=>$datos->parentesco1,'parentesco2'=>$datos->parentesco2,'parentesco3'=>$datos->parentesco3,'parentesco4'=>$datos->parentesco4,'porcentaje'=>$datos->porcentaje,'porcentaje1'=>$datos->porcentaje1,'porcentaje2'=>$datos->porcentaje2,'porcentaje3'=>$datos->porcentaje3,'porcentaje4'=>$datos->porcentaje4,'primaria'=>$datos->primaria,'duracion_primaria'=>$datos->duracion_primaria,'titulo_primaria'=>$datos->titulo_primaria,'secundaria'=>$datos->secundaria,'duracion_secundaria','titulo_secundaria'=>$datos->titulo_secundaria,'preparatoria'=>$datos->preparatoria,
+            'duracion_preparatoria'=>$datos->duracion_preparatoria,'titulo_preparatoria'=>$datos->titulo_preparatoria,'profesional'=>$datos->profesional,'duracion_profesional'=>$datos->duracion_profesional,'titulo_profesional'=>$datos->titulo_profesional,'otras'=>$datos->otras,'duracion_otras'=>$datos->duracion_otras,'titulo_otras'=>$datos->titulo_otras,'estudio_actual'=>$datos->estudio_actual,'carrera'=>$datos->carrera,'grado'=>$datos->grado,'horario'=>$datos->horario,'vinvulacion_trabajo'=>$datos->vinvulacion_trabajo,
     'desvinculacion_trabajo'=>$datos->desvinculacion_trabajo,'nombre_compania'=>$datos->nombre_compania,'direccion_compania'=>$datos->direccion_compania,'telefono_compania'=>$datos->telefono_compania,'sueldo'=>$datos->sueldo,'motivo_separacion'=>$datos->motivo_separacion,'nombre_jefe'=>$datos->nombre_jefe,'puesto_jefe'=>$datos->puesto_jefe,
     'solicitar_informes'=>$datos->solicitar_informes,'razones'=>$datos->razones,'vinculacion_trabajo1'=>$datos->vinculacion_trabajo1,'desvinculacion_trabajo1'=>$datos->desvinculacion_trabajo1,'nombre_compania1'=>$datos->nombre_compania1,'direccion_compania1'=>$datos->direccion_compania1,'telefono_compania1'=>$datos->telefono_compania1,'sueldo1'=>$datos->sueldo1,'motivo_separacion1'=>$datos->motivo_separacion1,'nombre_jefe1'=>$datos->nombre_jefe1,'puesto_jefe1'=>$datos->puesto_jefe1,'solicitar_informes1'=>$datos->solicitar_informes1,'razones1'=>$datos->razone1,'vinvulacion_trabajo2'=>$datos->vinvulacion_trabajo2, 'desvinculacion_trabajo2'=>$datos->desvinculacion_trabajo2,'nombre_compania2'=>$datos->nombre_compania2,'direccion_compania2'=>$datos->direccion_compania2,'telefono_compania2'=>$datos->telefono_compania2,'sueldo2'=>$datos->sueldo2,'motivo_separacion2'=>$datos->motivo_separacion2,'nombre_jefe2'=>$datos->nombre_jefe2,'puesto_jefe2'=>$datos->puesto_jefe2,'solicitar_informes2'=>$datos->solicitar_informes2,'razones2'=>$datos->razones2,'vinvulacion_trabajo3'=>$datos->vinvulacion_trabajo3,'desvinculacion_trabajo3'=>$datos->desvinculacion_trabajo3,'nombre_compania3'=>$datos->nombre_compania3, 'direccion_compania3'=>$datos->direccion_compania3,'telefono_compania3'=>$datos->telefono_compania3,'sueldo3'=>$datos->sueldo3,'motivo_separacion3'=>$datos->motivo_separacion3,'nombre_jefe3'=>$datos->nombre_jefe3,'puesto_jefe3'=>$datos->puesto_jefe3,'solicitar_informes3'=>$datos->solicitar_informes3,'razones3'=>$datos->razones3,'referencia'=>$datos->referencia,'direccion_trabajo'=>$datos->direccion_trabajo,'telefono_referencia'=>$datos->telefono_referencia,'ocupacion'=>$datos->ocupacion,'tiempo'=>$datos->tiempo,'referencia1'=>$datos->referencia1,'direccion_trabajo1'=>$datos->direccion_trabajo1,'telefono_referencia1'=>$datos->telefono_referencia1,'ocupacion1'=>$datos->ocupacion1,'tiempo1'=>$datos->tiempo1,'referencia2'=>$datos->referencia2,'direccion_trabajo2'=>$datos->direccion_trabajo2,'telefono_referencia2'=>$datos->telefono_referencia2,'ocupacion2'=>$datos->ocupacion2,'tiempo2'=>$datos->tiempo2,'tipo_trabajador'=>$datos->tipo_trabajador,'turno'=>$datos->turno,'contrato'=>$datos->contrato,'contrato_documento'=>$datos->contrato_documento,'vigencia'=>$datos->vigencia,'horario_trabajo'=>$datos->horario_trabajo,'sueldo_diario'=>$datos->sueldo_diario,'nivel'=>$datos->nivel,'categoria'=>$datos->categoria,'tipo_sueldo'=>$datos->tipo_sueldo, 'tipo_jornada'=>$datos->tipo_jornada,'dias'=>$datos->dias,'horas_diarias'=>$datos->horas_diarias,'forma_pago'=>$datos->forma_pago,'clave_banco'=>$datos->clave_banco,'tarjeta_banco'=>$datos->tarjeta_banco,'envio_correspondencia'=>$datos->envio_correspondencia,'ptu'=>$datos->ptu,'observaciones'=>$datos->observaciones,'salario_cotizacion'=>$datos->salario_cotizacion,'salario_anterior'=>$datos->salario_anterior,'causa_modificacion'=>$datos->causa_modificacion]);
         }
