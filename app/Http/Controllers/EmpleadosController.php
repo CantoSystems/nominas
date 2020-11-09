c346f06af700330b51b7b85a1548bb29b411f1ee
elizabeth <anely_lyzzye@hotmail.com>
2020-11-09 20:45:43 +0100

    Documentacion migracion controlador y rutas de Empresas

--- app/Http/Controllers/EmpleadosController.php
+++ app/Http/Controllers/EmpleadosController.php
@@ -14,27 +14,31 @@ use Illuminate\Http\UploadedFile;
 
 class EmpleadosController extends Controller
 {
+   
     public function conectar($clv)
-{
+    {
 
-    $configDb = [
-        'driver'      => 'mysql',
-        'host'        => env('DB_HOST', 'localhost'),
-        'port'        => env('DB_PORT', '3306'),
-        'database'    => $clv,
-        'username'    => env('DB_USERNAME', 'root'),
-        'password'    => env('DB_PASSWORD', ''),
-        'unix_socket' => env('DB_SOCKET', ''),
-        'charset'     => 'utf8',
-        'collation'   => 'utf8_unicode_ci',
-        'prefix'      => '',
-        'strict'      => true,
-        'engine'      => null,
-];
+        $configDb = [
+            'driver'      => 'mysql',
+            'host'        => env('DB_HOST', 'localhost'),
+            'port'        => env('DB_PORT', '3306'),
+            'database'    => $clv,
+            'username'    => env('DB_USERNAME', 'root'),
+            'password'    => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset'     => 'utf8',
+            'collation'   => 'utf8_unicode_ci',
+            'prefix'      => '',
+            'strict'      => true,
+            'engine'      => null,
+        ];
 
-return $configDb;
+        return $configDb;
+    }
 
-}
+
+
+
     public function index(Request $request){
     	$clv=Session::get('clave_empresa');
         $clv_empresa=$this->conectar($clv);
