b8c9dc9f0240d30454b5fbb42bffe79eebb07173
paablomont <jpomprime@gmail.com>
2020-11-13 09:08:01 -0600

    Modificaciones de Clasificaciones

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -111,14 +111,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </p>
             </a>
           </li>
-          <li class="nav-item">
+          <!--<li class="nav-item">
             <a href="{{ route('clasificacion.acciones')}}" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
                 Clasificaciones
               </p>
             </a>
-          </li>
+          </li>-->
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
