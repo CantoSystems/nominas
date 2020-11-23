497bea4ed7845a2ac52ced952c6defda6fab91aa
elizabeth <anely_lyzzye@hotmail.com>
2020-11-23 20:23:37 +0100

    link modal update y delete areas

--- resources/views/Areas/modalupdateareas.blade.php
+++ resources/views/Areas/modalupdateareas.blade.php
@@ -1,8 +1,8 @@
-<div class="modal fade" id="modalcrudempleado" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modalupdatearea" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   	<div class="modal-dialog modal-dialog-centered modal-xl">
     	<div class="modal-content">
       		<div class="modal-header">
-        		<h5 class="modal-title" id="exampleModalLabel">Registro delete</h5>
+        		<h5 class="modal-title" id="exampleModalLabel">Registro updates</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
