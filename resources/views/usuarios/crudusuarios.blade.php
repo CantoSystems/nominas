206bd78a75f5a7999ca23e059849bde75f252593
elizabeth <anely_lyzzye@hotmail.com>
2020-12-03 18:06:09 +0100

    front usuarios y ocultar seccion

---
+++ resources/views/usuarios/crudusuarios.blade.php
@@ -0,0 +1,177 @@
+@extends('layouts.principal')
+@section('content')
+<div class="container">
+    <div class="row">
+        <!-- Inicio Datatables-->
+        <div class="col">
+            <div class="card card-secondary">
+                <div class="card-header">
+                    <h3 class="card-title">Usuarios</h3>
+                </div>
+                <div class="card-body">
+                    <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Nombre del Role</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                           
+                            <tr>
+                                <th scope="row"></th>
+                                <td></td>
+                            </tr>
+                      
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+         <!--Fin Datatables-->
+
+         <!--Sección botones y direcciones-->
+        <div class="col">
+            <div class="card card-secondary">
+                <div class="card-header">
+                    <h3 class="card-title">Usuarios</h3>
+                </div>
+            <div class="card-body">
+                <form action="" method="GET" autocomplete="off">
+                        <div class="row">
+                          <div class="col-sm-6">
+                              <div class="form-group">
+                                  <label>Nombre del Usuario:</label>
+                                  <input type="text" name="name"  value="" class="form-control"  onkeyup="mayus(this);">
+
+                              </div>
+                          </div>
+
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Correo electronico:</label>
+                                    <input type="text" name="email"  value=""   class="form-control"  onkeyup="mayus(this)"; onkeypress="return validar(event)" >
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Contraseña:</label>
+                                    <input type="text" name="password"  value=""   class="form-control"  onkeyup="mayus(this)"; onkeypress="return validar(event)" >
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Confirmar contraseña:</label>
+                                    <input type="text" name="password_confirmation"  value=""   class="form-control"  onkeyup="mayus(this)"; onkeypress="return validar(event)" >
+                                </div>
+                            </div>
+
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <select  class="custom-select" name="">
+
+                                        <option  id="areas_null" value="null" style="text-align: center;">
+                                            -------- 
+                                            Seleccione una opción 
+                                             --------
+                                        </option>
+                                
+                                        <option  value=""></option>
+                                     </select>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-5">
+                                <div class="card-body">
+                                    <div class="margin">
+                                        <div class="btn-group">
+                                            <div class="form-group">
+                                                <button type="submit"  name="acciones" value="primero" id="primero" style='width:40px; height:27px'><i class="fas fa-backward" ></i></button>
+                                            </div>
+                                            <div class="form-group">
+                                            <button type="submit" name="acciones" value="atras" id="atras" style='width:40px; height:27px'><i class="fas fa-arrow-circle-left"></i></button>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" name="acciones" value="siguiente" id="siguiente" style='width:40px; height:27px'><i class="fas fa-arrow-circle-right"></i></button>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" name="acciones" value="ultimo" id="ultimo" style='width:40px; height:27px'><i class="fas fa-forward"></i></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="col-sm-4">
+                                <div class="card-body">
+                                    <div class="margin">
+                                        <div class="btn-group">
+                                            <div class="form-group">
+                                                <button id="buscar" type="button" data-toggle="modal" data-target="#exampleModal" style='width:40px; height:27px'>
+                                                    <i class="fas fa-search"></i>
+                                                  </button>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="button" id="nuevo" style='width:40px; height:27px'> <i class="fas fa-user-plus"></i></button>
+                                            </div>
+                                            <div class="form-group">
+                                             <button type="button" id="actualizar" style='width:40px; height:27px'> <i class="fas fa-pen-square"></i></button>
+                                            </div>
+                                            <div class="form-group">
+                                            <a id="eliminar" data-target="#delete" data-toggle="modal">
+                                            <button type="button" style='width:40px; height:27px'>
+                                            <i class="far fa-trash-alt">
+                                            </i>
+                                            </button></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3">
+                    <div class="card-body">
+                        <div class="margin">
+                            <div class="btn-group">
+                                <div class="form-group">
+
+                                       <button id="nuevo_reg" name="acciones" value="registrar" type="submit" style="display: none;width:40px; height:27px'"><i class="fas fa-save"></i></button>
+
+
+                                </div>
+                                <div class="form-group">
+                                    <button name="acciones" value="actualizar" id="actualizar_reg" type="submit" style="display: none;width:40px; height:27px'"><i class="fas fa-save"></i></button>
+                                </div>
+
+                                <div class="form-group">
+                                    <button name="acciones" value="cancelar_banco" id="cancelar_reg" type="submit" style="display: none;width:40px; height:28px"><i class="far fa-window-close"></i></button>
+                                </div>
+
+                                <div class="form-group">
+                                    <button name="acciones" value="cancelar_actualiza" id="cancelar_actual" type="submit" style="display: none;width:40px; height:28px"><i class="far fa-window-close"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                        </div>
+                    </form>
+                    @include('usuarios.modaldeleteusuarios')
+                    @include('usuarios.modalsearchusuarios')
+                     
+                
+                   </div>
+                   </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
+
+@endsection
