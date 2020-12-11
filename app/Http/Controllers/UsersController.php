fda0d2f54329524abfa61e834353fe5ff8527491
elizabeth <anely_lyzzye@hotmail.com>
2020-12-11 02:16:14 +0100

    direcciones botones modificacion register

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,7 +16,8 @@ class UsersController extends Controller
     public function index(Request $request)
     {   
         $accion = $request->acciones;
-        $clv = $request->id;
+        $clv = $request->email;
+     
         switch ($accion) {
             case '':
                 $roles = Role::all();
@@ -27,6 +28,44 @@ class UsersController extends Controller
                 ->join('roles','roles.id','=','users.role_id')
                 ->get();
                 return view('usuarios.crudusuarios',compact('roles','usuarios','data_user'));
+                break;
+
+            case 'atras':
+                $identificador = User::where('email',$clv)->first();           
+                $ide = $identificador->id;
+                $usuarios = User::select('users.*','roles.*')
+                    ->join('roles','roles.id','=','users.role_id')
+                    ->where('users.id','<',$ide)
+                    ->orderBy('users.id','desc')
+                    ->first();
+                if(is_null($usuarios)){
+                    $usuarios = User::select('users.*','roles.*')
+                ->join('roles','roles.id','=','users.role_id')
+                ->get()->last();
+                }
+                $roles = Role::all();
+                $data_user = User::select('users.*','roles.*')
+                ->join('roles','roles.id','=','users.role_id')
+                ->get();
+                return view('usuarios.crudusuarios',compact('roles','usuarios','data_user'));
+                break;
+            case 'siguiente':
+                $ident = User::where('email',$clv)->first();
+                $usuarios = User::select('users.*','roles.*')
+                ->join('roles','roles.id','=','users.role_id')
+                ->where('users.id','>',$ident->id)->first();
+                if(is_null($usuarios)){
+                    $usuarios = User::select('users.*','roles.*')
+                ->join('roles','roles.id','=','users.role_id')
+                ->first();
+                }
+                $roles = Role::all();
+                $data_user = User::select('users.*','roles.*')
+                ->join('roles','roles.id','=','users.role_id')
+                ->get();
+                return view('usuarios.crudusuarios',compact('roles','usuarios','data_user'));
+
+
                 break;
             
             default:
