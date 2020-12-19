bd206fd28dd80d82fe94489536d87f6c379ab81f
elizabeth <anely_lyzzye@hotmail.com>
2020-12-19 07:56:24 +0100

    empresas y validaciones personalizadas

--- resources/views/empresas/crudempresas.blade.php
+++ resources/views/empresas/crudempresas.blade.php
@@ -10,97 +10,166 @@
     			<div class="col-sm-6">
                     <div class="form-group">
                         <label>Nombre:</label>
-                        <input type="text" name="nombre"  class="form-control" value="{{$empresa->nombre}}" onkeyup="mayus(this)"; onkeypress="return validar(event)" >
+                        @if(isset($empresa))
+                            <input type="text" name="nombre"  class="form-control" value="{{$empresa->nombre}}" onkeyup="mayus(this)"; onkeypress="return validar(event)">
+                        @else
+                            <input type="text" name="nombre"  class="form-control" value="" onkeyup="mayus(this)"; onkeypress="return validar(event)">
+                        @endif
                     </div>
                 </div>    
                 <div class="col-sm-1">
                     <div class="form-group">
                         <label>Clave:</label>
-                        <input type="text" name="clave" class="form-control" value="{{$empresa->clave}}" readonly onkeyup="mayus(this);">
+                        @if(isset($empresa))
+                            <input type="text" name="clave" class="form-control" value="{{$empresa->clave}}" readonly onkeyup="mayus(this);">
+                        @else
+                            <input type="text" name="clave" class="form-control" value="" readonly onkeyup="mayus(this);">
+                        @endif
+
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label>RFC (Empresa):</label>
-                        <input type="text" name="rfc" maxlength="13" minlength="12" class="form-control" value="{{$empresa->rfc}}" onkeyup="mayus(this);" pattern="^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([A-Z]|[0-9]){2}([A-ZÑ\x26]|[0-9]){1})?$">
+                        @if(isset($empresa))
+                            <input type="text" name="rfc" maxlength="13" minlength="12" class="form-control" value="{{$empresa->rfc}}" onkeyup="mayus(this);" pattern="^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([A-Z]|[0-9]){2}([A-ZÑ\x26]|[0-9]){1})?$">
+                        @else
+                            <input type="text" name="rfc" maxlength="13" minlength="12" class="form-control" value="" onkeyup="mayus(this);" pattern="^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([A-Z]|[0-9]){2}([A-ZÑ\x26]|[0-9]){1})?$">
+                        @endif
+
                     </div>
                 </div>
                 <div class="col-sm-2">
                     <div class="form-group">
                         <label>Seguro social:</label>
-                        <input type="text" name="segurosocial" maxlength="11" class="form-control" value="{{$empresa->segurosocial}}" onkeyup="mayus(this)"; onkeypress="return numeros(event)" >
+                        @if(isset($empresa))
+                            <input type="text" name="segurosocial" maxlength="11" class="form-control" value="{{$empresa->segurosocial}}" onkeyup="mayus(this)"; onkeypress="return numeros(event)">
+                        @else
+                            <input type="text" name="segurosocial" maxlength="11" class="form-control" value="" onkeyup="mayus(this)"; onkeypress="return numeros(event)">
+                        @endif
+
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label>Registro estatal ( REC ):</label>
-                        <input type="text" name="registro_estatal" class="form-control" value="{{$empresa->registro_estatal}}" maxlength="11"onkeyup="mayus(this);">
+                        @if(isset($empresa))
+                            <input type="text" name="registro_estatal" class="form-control" value="{{$empresa->registro_estatal}}" maxlength="11"onkeyup="mayus(this);">
+                        @else
+                            <input type="text" name="registro_estatal" class="form-control" value="" maxlength="11"onkeyup="mayus(this);">
+                        @endif
                     </div>
                 </div>
                 <div class="col-sm-5">
                     <div class="form-group">
                         <label>Calle:</label>
-                        <input type="text" name="calle" class="form-control" value="{{$empresa->calle}}" onkeyup="mayus(this)"; onkeypress="return validar(event)">
+                        @if(isset($empresa))
+                            <input type="text" name="calle" class="form-control" value="{{$empresa->calle}}" onkeyup="mayus(this)"; onkeypress="return validar(event)">
+                        @else
+                            <input type="text" name="calle" class="form-control" value="" onkeyup="mayus(this)"; onkeypress="return validar(event)">
+                        @endif
                     </div>
                 </div>
                 <div class="col-sm-2">
                     <div class="form-group">
                         <label>Número interno:</label>
-                        <input type="text" name="num_interno" max="6000" min="1" class="form-control" value="{{$empresa->num_interno}}" onkeypress="return numeros(event)" >
+                        @if(isset($empresa))
+                            <input type="text" name="num_interno" max="6000" min="1" class="form-control" value="{{$empresa->num_interno}}" onkeypress="return numeros(event)">
+                        @else
+                            <input type="text" name="num_interno" max="6000" min="1" class="form-control" value="" onkeypress="return numeros(event)">
+                        @endif
                     </div>
                 </div>
                 <div class="col-sm-2">
                     <div class="form-group">
                         <label>Número externo:</label>
-                        <input type="text" name="num_externo" class="form-control" value="{{$empresa->num_externo}}" onkeypress="return numeros(event)">
+                        @if(isset($empresa))
+                            <input type="text" name="num_externo" class="form-control" value="{{$empresa->num_externo}}" onkeypress="return numeros(event)">
+                        @else
+                            <input type="text" name="num_externo" class="form-control" value="" onkeypress="return numeros(event)">
+                        @endif
                     </div>
                 </div>
                 <div class="col-sm-5">
                     <div class="form-group">
                         <label>Colonia:</label>
-                        <input type="text" name="colonia" class="form-control" value="{{$empresa->colonia}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @if(isset($empresa))
+                            <input type="text" name="colonia" class="form-control" value="{{$empresa->colonia}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @else
+                            <input type="text" name="colonia" class="form-control" value="" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @endif
+
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label>Municipio:</label>
-                        <input type="text" name="municipio" class="form-control" value="{{$empresa->municipio}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @if(isset($empresa))
+                            <input type="text" name="municipio" class="form-control" value="{{$empresa->municipio}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @else
+                            <input type="text" name="municipio" class="form-control" value="" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @endif
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="form-group">
                         <label>Ciudad:</label>
-                        <input type="text" name="ciudad" class="form-control" value="{{$empresa->ciudad}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @if(isset($empresa))
+                            <input type="text" name="ciudad" class="form-control" value="{{$empresa->ciudad}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @else
+                            <input type="text" name="ciudad" class="form-control" value="" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @endif
+
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="form-group">
                         <label>País:</label>
-                        <input type="text" name="pais" class="form-control" value="{{$empresa->pais}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @if(isset($empresa))
+                            <input type="text" name="pais" class="form-control" value="{{$empresa->pais}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @else
+                            <input type="text" name="pais" class="form-control" value="" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @endif
                     </div>
                 </div>
                 <div class="col-sm-5">
                     <div class="form-group">
                         <label>Representante legal:</label>
-                        <input type="text" name="representante_legal" maxlength="20" class="form-control" value="{{$empresa->representante_legal}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @if(isset($empresa))
+                            <input type="text" name="representante_legal" maxlength="20" class="form-control" value="{{$empresa->representante_legal}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @else
+                            <input type="text" name="representante_legal" maxlength="20" class="form-control" value="" onkeyup="mayus(this);" onkeypress="return validar(event)">
+                        @endif
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label>RFC (Representante legal):</label>
-                        <input type="text" name="rfc_representante" maxlength="13" class="form-control" value="{{$empresa->rfc_representante}}" onkeyup="mayus(this);" pattern="^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([A-Z]|[0-9]){2}([A-ZÑ\x26]|[0-9]){1})?$">
+                        @if(isset($empresa))
+                            <input type="text" name="rfc_representante" maxlength="13" class="form-control" value="{{$empresa->rfc_representante}}" onkeyup="mayus(this);" pattern="^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([A-Z]|[0-9]){2}([A-ZÑ\x26]|[0-9]){1})?$">
+                        @else
+                            <input type="text" name="rfc_representante" maxlength="13" class="form-control" value="" onkeyup="mayus(this);" pattern="^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([A-Z]|[0-9]){2}([A-ZÑ\x26]|[0-9]){1})?$">
+                        @endif
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="form-group">
                         <label>Teléfono:</label>
-                        <input type="text" name="telefono" id="telefono" max="10" class="form-control telefono" value="{{$empresa->telefono}}" onkeypress="return numeros(event)">
+                        @if(isset($empresa))
+                            <input type="text" name="telefono" id="telefono" max="10" class="form-control telefono" value="{{$empresa->telefono}}" onkeypress="return numeros(event)">
+                        @else
+                            <input type="text" name="telefono" id="telefono" max="10" class="form-control telefono" value="" onkeypress="return numeros(event)">
+                        @endif
                     </div>
                 </div>
                 <div class="col-sm-8">
                     <div class="form-group">
                         <label>Correo electrónico:</label>
-                        <input type="email" name="email" class="form-control" value="{{$empresa->email}}">
+                        @if(isset($empresa))
+                            <input type="email" name="email" class="form-control" value="{{$empresa->email}}">
+                        @else
+                            <input type="email" name="email" class="form-control" value="">
+                        @endif
                     </div>
                 </div>
 
@@ -109,6 +178,7 @@
                     <div class="card-body">
                         <div class="margin">
                             <div class="btn-group">
+                            @isset($empresa)
                                 <div class="form-group">
                                     <button type="submit"  name="acciones" value="primero" id="primero" style='width:70px; height:40px'><i class="fas fa-backward" ></i></button>
                                 </div>
@@ -121,6 +191,7 @@
                                 <div class="form-group">
                                     <button type="submit" name="acciones" value="ultimo" id="ultimo" style='width:70px; height:40px'><i class="fas fa-forward"></i></button>
                                 </div>
+                            @endisset
                             </div>
                         </div>
                     </div>
@@ -138,17 +209,21 @@
                                 </div>
                                 @endcanany
                                 @canany(['administrador','capturista'])
-                                <div class="form-group">
-                                 <button type="button" id="actualizar" style='width:70px; height:40px'> <i class="fas fa-pen-square"></i></button>
-                                </div>
+                                    @isset($empresa)
+                                        <div class="form-group">
+                                            <button type="button" id="actualizar" style='width:70px; height:40px'> <i class="fas fa-pen-square"></i></button>
+                                        </div>
+                                    @endisset
                                 @endcanany
                                 @can('administrador')
                                 <div class="form-group">
+                                    @isset($empresa)
                                     <a href="#" id="eliminar" data-target="#modal-deletenom-{{$empresa->id}}" data-toggle="modal" style='width:70px; height:40px'>
                                         <button type="button" style='width:70px; height:40px'>
                                             <i class="far fa-trash-alt"></i>
                                         </button>
                                     </a>
+                                    @endif
                                 </div>
                                 @endcan
                             </div>
@@ -176,8 +251,10 @@
                     </div>
                 </div>
             </div>	
-    	</form>	
-        @include('empresas.modaldelete')
+    	</form>
+        @isset($empresa)
+            @include('empresas.modaldelete')
+        @endisset
    	</div> 	
 </div>	
 @endsection
