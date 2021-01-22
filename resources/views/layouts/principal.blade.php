10beb771a1aee6ed43fc9ce93c73ad1a5e9e8f21
paablomont <jpomprime@gmail.com>
2021-01-21 22:56:51 -0600

    Subsidios2

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -358,6 +358,25 @@ scratch. This page gets rid of all links and provides the needed markup only.
       "info": true,
       "autoWidth": false,
       "responsive": true,
+      language: {
+        "decimal": "",
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Entradas",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
     });
   });
 </script>
