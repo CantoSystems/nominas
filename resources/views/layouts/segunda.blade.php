0523258cac7d86ab7949893e206050b9b5aa6514
Elizabeth <anely_lyzzye@hotmail.com>
2021-02-24 20:42:49 +0100

    fecha en funciones

--- resources/views/layouts/segunda.blade.php
+++ resources/views/layouts/segunda.blade.php
@@ -478,6 +478,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
   $(document).on('click', '.borrar', function (event) {
     event.preventDefault();
     $(this).closest('tr').remove();
+    var fecha = new Date(); //Fecha actual
+    var mes = fecha.getMonth()+1; //obteniendo mes
+    var dia = fecha.getDate()-1; //obteniendo dia
+    var ano = fecha.getFullYear(); //obteniendo año
+    if(dia<10)
+     dia='0'+dia; //agrega cero si el menor de 10
+    if(mes<10)
+      mes='0'+mes //agrega cero si el menor de 10
+    document.getElementById('fecha_extra').value=ano+"-"+mes+"-"+dia;
   });
 
   $(document).ready(function(){
@@ -560,6 +569,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
       $('input[type="text"]').val('');
       $('input[type="date"]').val('');
       $('input[type="number"]').val('');
+    var fecha = new Date(); //Fecha actual
+    var mes = fecha.getMonth()+1; //obteniendo mes
+    var dia = fecha.getDate()-1; //obteniendo dia
+    var ano = fecha.getFullYear(); //obteniendo año
+    if(dia<10)
+     dia='0'+dia; //agrega cero si el menor de 10
+    if(mes<10)
+      mes='0'+mes //agrega cero si el menor de 10
+    document.getElementById('fecha_extra').value=ano+"-"+mes+"-"+dia;
     });
   });
 
