0bf5ac24f732f6ba5a98daf66a29d8259e4204d5
paablomont <jpomprime@gmail.com>
2021-03-29 15:07:17 -0600

    Generación Periodo automático al crear empresa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,26 @@
 @extends('layouts.principal')
-
 @section('content') 
-
-
 <div class="card">
-    <div class="card-header">
-      Empresas
-    </div>
-    <div class="card-body">
-        <form action="{{route('seleccionarempresa')}}" method="GET">
-        <div class="col-md-12">
-            <h5 class="card-title">Selecciona la nomina con la que se trabajara:</h5>
-        </div>
+  <div class="card-header">
+    Empresas
+  </div>
+  <div class="card-body">
+    <form action="{{route('seleccionarempresa')}}" method="GET">
+      <div class="col-md-12">
+        <h5 class="card-title">Selecciona la nómina con la que se trabajará:</h5>
+      </div>
       <br>
       <div class="row">
         <div class="col-md-8">
-            <select class="custom-select" name="empresa">
-                @foreach($empresas as $empresa)
-                  <option value="{{$empresa->clave}}">{{$empresa->nombre}}  {{$empresa->nombre_nomina}} </option> 
-                @endforeach
-            </select>
-         </div>    
-         <button type="submit">Seleccionar</button>
-        </form>
+          <select class="custom-select" name="empresa">
+            @foreach($empresas as $empresa)
+              <option value="{{$empresa->clave}}">{{$empresa->nombre}} | {{$empresa->nombre_nomina}} </option> 
+            @endforeach
+          </select>
+        </div>    
+        <button type="submit">Seleccionar</button>
       </div>
-
-    </div>
+    </form>
   </div>
+</div>
 @endsection
--- resources/views/periodos/periodos.blade.php
+++ resources/views/periodos/periodos.blade.php
@@ -1,34 +1,27 @@
 @extends('layouts.principal')
-
 @section('content')
 <div class="card">
-    <div class="card-header">
-      Periodos
-    </div>
-    <div class="card-body">
-      <h5 class="card-title">Selecciona el periodo con el que trabajaras: </h5><br>
-      
-      <br>
-      <div class="row">
-        
-          <form action="{{route('seleccionarperiodo')}}" method="GET">
-
-            <div class="col-md-12">
-              <select class="custom-select" name="periodo">
-                @foreach ($periodos as $periodo)
-                <option value={{$periodo->numero}}> DE: {{$periodo->fecha_inicio}} A {{$periodo->fecha_fin}}</option> 
-                @endforeach
-              </select>
-            </div><br> 
-            
-            <div class="col-md-6">
-              <button type="submit" style="width:220px; height:40px">Seleccionar</button>
-            </div>
-          </form>
-
-        </div>
-      </div> 
-    </div>
+  <div class="card-header">
+    Periodos
   </div>
+  <div class="card-body">
+    <h5 class="card-title">Selecciona el periodo con el que trabajarás:</h5><br>
+    <br>
+    <div class="row">
+      <form action="{{route('seleccionarperiodo')}}" method="GET">
+        <div class="col-md-12">
+          <select class="custom-select" name="periodo">
+            @foreach($periodos as $periodo)
+              <option value="{{$periodo->numero}}"> DE: {{$periodo->fecha_inicio}} A {{$periodo->fecha_fin}}</option> 
+            @endforeach
+          </select>
+        </div>
+        <br> 
+        <div class="col-md-6">
+          <button type="submit" style="width:220px; height:40px">Seleccionar</button>
+        </div>
+      </form>
+    </div> 
+  </div>
+</div>
 @endsection
-
