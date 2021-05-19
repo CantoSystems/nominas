f6a64b607fdbba4bf5510bf3e0872e2e496726a4
paablomont <jpomprime@gmail.com>
2021-05-19 09:49:36 -0500

    Nueva vista Prenomina

--- resources/views/prenomina/prenomina.blade.php
+++ resources/views/prenomina/prenomina.blade.php
@@ -12,47 +12,49 @@
                         </h3>
                     </div>
                     <div class="card-body">
-                        <table id="example1" class="table table-bordered table-striped">
-                        <input type="hidden" id="numEmp" name="numEmp" value="{{ $numEmp }}" >
-                            <meta name="csrf-token" content="{{ csrf_token() }}">
-                            <thead>
-                                <tr>
-                                    <th style="width: 15px; text-align: center;">Clave</th>
-                                    <th>Empleado</th>
-                                    <th>Departamento</th>
-                                    <th style="text-align: center;">Total Percepciones</th>
-                                    <th style="text-align: center;">Total Deducciones</th>
-                                    <th style="text-align: center;">Pago Neto</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @if(!empty($empleados))
-                                    @foreach($empleados as $emplea)
-                                        <tr>
-                                            <td style="width: 15px; text-align: center;" scope="row">{{ $emplea->clave_empleado }}</td>
-                                            <td>{{ $emplea->nombre}} {{ $emplea->apellido_paterno }} {{ $emplea->apellido_materno }}</td>
-                                            <td>{{ $emplea->departamento }}</td>
-                                            <td style="text-align: center;">$7,500.00</td>
-                                            <td style="text-align: center;">$7,500.00</td>
-                                            <td style="text-align: center;">$7,500.00</td>
-                                            <td style="width: 15px;">
-                                                @can('administrador')
-                                                    <a data-target="#calculo-prenomina{{$emplea->id_emp}}" data-toggle="modal" style='width:70px; height:40px'>
-                                                        <button type="button" class="extraer_id" style='width: 38px; height: 25px'>
-                                                            <i class="far fa-eye"></i>
-                                                            <input type="hidden" class="identificador_prenomina" value="{{ $emplea->id_emp }}" name="">
-                                                        </button>
-                                                    </a>
-                                                    <!--Incluye los modales por cada accion de eliminar generando sean diferentes los ID -->
-                                                    @include('prenomina.calculo-prenomina')                
-                                                @endcan
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @endif
-                            </tbody>
-                        </table>
+                        <form action="{{ route('prenomina.index' )}}" method="GET" autocomplete="off">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 15px; text-align: center;">Clave</th>
+                                        <th>Empleado</th>
+                                        <th>Departamento</th>
+                                        <th style="text-align: center;">Total Percepciones</th>
+                                        <th style="text-align: center;">Total Deducciones</th>
+                                        <th style="text-align: center;">Pago Neto</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @if(!empty($empleados))
+                                        @foreach($empleados as $emplea)
+                                            <tr>
+                                                <td style="width: 15px; text-align: center;" scope="row">{{ $emplea->clave_empleado }}</td>
+                                                <td>{{ $emplea->nombre}} {{ $emplea->apellido_paterno }} {{ $emplea->apellido_materno }}</td>
+                                                <td>{{ $emplea->departamento }}</td>
+                                                <td style="text-align: center;">${{ $emplea->total_percepciones }}</td>
+                                                <td style="text-align: center;">${{ $emplea->total_deducciones }}</td>
+                                                <td style="text-align: center;">${{ $emplea->total_percepciones - $emplea->total_deducciones - $emplea->total_impuestosTrabajador - $emplea->total_impuestosPatron }}</td>
+                                                <td style="width: 15px;">
+                                                    @can('administrador')
+                                                        <a data-target="#calculo-prenomina{{$emplea->id_emp}}" data-toggle="modal" style='width:70px; height:40px'>
+                                                            <button type="button" class="extraer_id" style='width: 38px; height: 25px'>
+                                                                <i class="far fa-eye"></i>
+                                                                <input type="hidden" class="identificador_prenomina" value="{{ $emplea->id_emp }}" name="">
+                                                            </button>
+                                                        </a>
+                                                        @include('prenomina.calculo-prenomina')                
+                                                    @endcan
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    @endif
+                                </tbody>
+                            </table>
+                            <center>
+                                <button type="submit" name="acciones" value="calcular" id="calcular" style='width:125px; height:25px;'>Calcular Nómina</button>
+                            </center>
+                        </form>
                     </div>
                 </div>
             </div>
