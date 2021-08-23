989b8b96dc3c84328fb9f7744c3e6002465ce930
Elizabeth <anely_lyzzye@hotmail.com>
2021-08-23 16:13:33 -0500

    actualizacion campos

--- database/seeds/RetencionesSeeder.php
+++ database/seeds/RetencionesSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
@@ -15,28 +15,28 @@ class RetencionesSeeder extends Seeder
     public function run()
     {
         $retenciones = Collect([
-            ["limite_inferior" => 0.01, "limite_superior" => 644.58, "cuota" => 0, "porcentaje_excedente"=>1.92,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
-            ["limite_inferior" => 644.59, "limite_superior" => 5470.92, "cuota" =>  12.38, "porcentaje_excedente"=>6.40,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
-            ["limite_inferior" => 5470.93, "limite_superior" => 9614.66, "cuota" => 321.26, "porcentaje_excedente"=>10.88,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
-            ["limite_inferior" => 9614.67, "limite_superior" => 11176.62, "cuota" => 772.10, "porcentaje_excedente"=>16.00,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
-            ["limite_inferior" => 11176.63, "limite_superior" => 13381.47, "cuota" => 1022.01, "porcentaje_excedente"=>17.92,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
-            ["limite_inferior" => 13381.48, "limite_superior" =>  26988.50, "cuota" => 1417.12, "porcentaje_excedente"=>21.36,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
-            ["limite_inferior" => 26988.51, "limite_superior" => 42537.58, "cuota" => 4323.58, "porcentaje_excedente"=>23.52,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
-            ["limite_inferior" => 42537.59, "limite_superior" =>  81211.25, "cuota" => 7980.73, "porcentaje_excedente"=>30.00,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
-            ["limite_inferior" => 81211.26, "limite_superior" => 108281.67, "cuota" => 28245.36, "porcentaje_excedente"=>340.00,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
-            ["limite_inferior" => 108281.68, "limite_superior" => 324845.01, "cuota" => 28245.36, "porcentaje_excedente"=>34.00,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
-            ["limite_inferior" => 324845.02, "limite_superior" => 0.00, "cuota" => 101876.90, "porcentaje_excedente"=>35.00,"created_at" => "2021-05-04 07:06:32", "updated_at" => "2021-05-04 07:06:32"],
+            ["limite_inferior" => 0.01, "limite_superior" => 644.58, "cuota" => 0, "porcentaje_excedente"=>1.92],
+            ["limite_inferior" => 644.59, "limite_superior" => 5470.92, "cuota" =>  12.38, "porcentaje_excedente"=>6.40],
+            ["limite_inferior" => 5470.93, "limite_superior" => 9614.66, "cuota" => 321.26, "porcentaje_excedente"=>10.88],
+            ["limite_inferior" => 9614.67, "limite_superior" => 11176.62, "cuota" => 772.10, "porcentaje_excedente"=>16.00],
+            ["limite_inferior" => 11176.63, "limite_superior" => 13381.47, "cuota" => 1022.01, "porcentaje_excedente"=>17.92],
+            ["limite_inferior" => 13381.48, "limite_superior" =>  26988.50, "cuota" => 1417.12, "porcentaje_excedente"=>21.36],
+            ["limite_inferior" => 26988.51, "limite_superior" => 42537.58, "cuota" => 4323.58, "porcentaje_excedente"=>23.52],
+            ["limite_inferior" => 42537.59, "limite_superior" =>  81211.25, "cuota" => 7980.73, "porcentaje_excedente"=>30.00],
+            ["limite_inferior" => 81211.26, "limite_superior" => 108281.67, "cuota" => 28245.36, "porcentaje_excedente"=>340.00],
+            ["limite_inferior" => 108281.68, "limite_superior" => 324845.01, "cuota" => 28245.36, "porcentaje_excedente"=>34.00],
+            ["limite_inferior" => 324845.02, "limite_superior" => 0.00, "cuota" => 101876.90, "porcentaje_excedente"=>35.00],
         ]);
-
+        $fecha = now()->toDateString();
         foreach($retenciones as $reten){
             DB::table('retenciones')->insert([
                 'limite_inferior' => $reten['limite_inferior'],
                 'limite_superior' => $reten['limite_superior'],
                 'cuota_fija' => $reten['cuota'],
                 'porcentaje_excedente'=>$reten['porcentaje_excedente'],
-                'periodo_retencion' => "ANUAL",
-                'created_at' => $reten['created_at'],
-                'updated_at' => $reten['updated_at']
+                'periodo_retencion' => "MENSUAL",
+                'created_at' => $fecha,
+                'updated_at' => $fecha
             ]);
 
         }
