d3f10041461340c0f5abaf6a10def84e86c8eeed
Elizabeth <anely_lyzzye@hotmail.com>
2022-05-30 13:09:30 -0500

    referencia a campos obligatorios

--- resources/views/empresas/crudempresas.blade.php
+++ resources/views/empresas/crudempresas.blade.php
@@ -15,7 +15,9 @@
     		<div class="row">
     			<div class="col-sm-4">
                     <div class="form-group">
-                        <label>Nombre:</label>
+                        <label>
+                            Nombre <strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="nombre"  class="form-control" value="{{$empresa->nombre}}" onkeyup="mayus(this)"; onkeypress="return validar(event)">
                             @error('nombre')
@@ -35,7 +37,9 @@
                 </div>    
                 <div class="col-sm-1">
                     <div class="form-group">
-                        <label>Clave:</label>
+                        <label>
+                            Clave <strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="clave" class="form-control" value="{{$empresa->clave}}" maxlength="4" onkeyup="mayus(this);">
                             @error('clave')
@@ -56,7 +60,9 @@
                 </div>
                 <div class="col-sm-4">
                     <div class="form-group">
-                        <label>Nombre Nómina:</label>
+                        <label>
+                            Nombre Nómina<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="nombre_nomina" class="form-control" value="{{$empresa->nombre_nomina}}" onkeyup="mayus(this);">
                             @error('nombre_nomina')
@@ -76,7 +82,9 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>RFC (Empresa):</label>
+                        <label>
+                            RFC (Empresa)<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="rfc" maxlength="13" minlength="12" class="form-control" value="{{$empresa->rfc}}" onkeyup="mayus(this);" pattern="^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([A-Z]|[0-9]){2}([A-ZÑ\x26]|[0-9]){1})?$">
                             @error('rfc')
@@ -96,7 +104,9 @@
                 </div>
                 <div class="col-sm-2">
                     <div class="form-group">
-                        <label>Registro patronal:</label>
+                        <label>
+                            Registro patronal<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="segurosocial" maxlength="11" class="form-control" value="{{$empresa->segurosocial}}" onkeyup="mayus(this)"; onkeypress="return numeros(event)">
                             @error('segurosocial')
@@ -116,27 +126,19 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>Registro estatal ( REC ):</label>
+                        <label>Registro estatal ( REC )</label>
                         @if(isset($empresa))
                             <input type="text" name="registro_estatal" class="form-control" value="{{$empresa->registro_estatal}}" maxlength="11"onkeyup="mayus(this);">
-                            @error('registro_estatal')
-                                <div class="alert alert-secondary">
-                                    {{ $message }}
-                                </div>
-                            @enderror
                         @else
                             <input type="text" name="registro_estatal" class="form-control" value="" maxlength="11"onkeyup="mayus(this);">
-                            @error('registro_estatal')
-                                <div class="alert alert-secondary">
-                                    {{ $message }}
-                                </div>
-                            @enderror
                         @endif
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>Calle:</label>
+                        <label>
+                            Calle<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="calle" class="form-control" value="{{$empresa->calle}}" onkeyup="mayus(this)"; onkeypress="return validar(event)">
                             @error('calle')
@@ -156,7 +158,9 @@
                 </div>
                 <div class="col-sm-2">
                     <div class="form-group">
-                        <label>Número exterior:</label>
+                        <label>
+                            Número exterior<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="num_externo" maxlength="40" class="form-control" value="{{$empresa->num_externo}}" onkeyup="mayus(this)";>
                             @error('num_externo')
@@ -176,7 +180,7 @@
                 </div>
                 <div class="col-sm-2">
                     <div class="form-group">
-                        <label>Número interior:</label>
+                        <label>Número interior</label>
                         @if(isset($empresa))
                             <input type="text" name="num_interno" maxlength="40" class="form-control" value="{{$empresa->num_interno}}" onkeyup="mayus(this)";>
                         @else
@@ -186,7 +190,9 @@
                 </div>
                 <div class="col-sm-2">
                     <div class="form-group">
-                        <label>Colonia:</label>
+                        <label>
+                            Colonia<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="colonia" class="form-control" value="{{$empresa->colonia}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
                             @error('colonia')
@@ -206,7 +212,9 @@
                 </div>
                 <div class="col-sm-2">
                     <div class="form-group">
-                        <label>Código postal:</label>
+                        <label>
+                            Código postal<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="codigopostal" class="form-control" value="{{$empresa->codigopostal}}" onkeyup="mayus(this);" onkeypress="return validar(event)" maxlength="5">
                             @error('codigopostal')
@@ -226,7 +234,9 @@
                 </div>
                 <div class="col-sm-2">
                     <div class="form-group">
-                        <label>Municipio:</label>
+                        <label>
+                            Municipio<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="municipio" class="form-control" value="{{$empresa->municipio}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
                             @error('municipio')
@@ -246,7 +256,9 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>Ciudad:</label>
+                        <label>
+                            Ciudad<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="ciudad" class="form-control" value="{{$empresa->ciudad}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
                             @error('ciudad')
@@ -266,7 +278,9 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>País:</label>
+                        <label>
+                            País<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="pais" class="form-control" value="{{$empresa->pais}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
                             @error('pais')
@@ -286,7 +300,9 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>Representante legal:</label>
+                        <label>
+                            Representante legal<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="representante_legal" maxlength="20" class="form-control" value="{{$empresa->representante_legal}}" onkeyup="mayus(this);" onkeypress="return validar(event)">
                             @error('representante_legal')
@@ -306,7 +322,9 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>RFC (Representante legal):</label>
+                        <label>
+                            RFC (Representante legal)<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <input type="text" name="rfc_representante" maxlength="13" class="form-control" value="{{$empresa->rfc_representante}}" onkeyup="mayus(this);" pattern="^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([A-Z]|[0-9]){2}([A-ZÑ\x26]|[0-9]){1})?$">
                             @error('rfc_representante')
@@ -326,41 +344,43 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>Teléfono:</label>
+                        <label>Teléfono</label>
                         @if(isset($empresa))
                             <input type="text" name="telefono" id="telefono" max="10" class="form-control telefono" value="{{$empresa->telefono}}" onkeypress="return numeros(event)">
-                            @error('telefono')
-                                <div class="alert alert-secondary">
-                                    {{ $message }}
-                                </div>
-                            @enderror
                         @else
                             <input type="text" name="telefono" id="telefono" max="10" class="form-control telefono" value="" onkeypress="return numeros(event)">
-                            @error('telefono')
-                                <div class="alert alert-secondary">
-                                    {{ $message }}
-                                </div>
-                            @enderror
                         @endif
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>Correo electrónico:</label>
+                        <label>Correo electrónico</label>
                         @if(isset($empresa))
                             <input type="email" name="email" class="form-control" value="{{$empresa->email}}">
-                            @error('email')
-                                <div class="alert alert-secondary">
-                                    {{ $message }}
-                                </div>
-                            @enderror
                         @else
                             <input type="email" name="email" class="form-control" value="">
-                            @error('email')
-                                <div class="alert alert-secondary">
-                                    {{ $message }}
-                                </div>
-                            @enderror
+                        @endif
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-group">
+                        <label>
+                            CURP Representante Legal<strong style="color: red">*</strong>
+                        </label>
+                        @if(isset($empresa))
+                            <input type="text" name="curpRepresentante" class="form-control" value="{{ $empresa->curpRepresentante}}" >
+                            @error('curpRepresentante')
+                            <div class="alert alert-secondary">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                        @else
+                            <input type="text" name="curpRepresentante" class="form-control">
+                            @error('curpRepresentante')
+                            <div class="alert alert-secondary">
+                                {{ $message }}
+                            </div>
+                        @enderror
                         @endif
                     </div>
                 </div>
@@ -368,7 +388,9 @@
               
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>Área Geográfica:</label>
+                        <label>
+                            Área Geográfica<strong style="color: red">*</strong>
+                        </label>
                         @if(isset($empresa))
                             <select id="regionSalario" name="regionEmpresa" class="custom-select">
                                 @if($empresa->region=="N/A")
@@ -406,7 +428,7 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>Factor Prima de Riesgo de Trabajo:</label>
+                        <label>Factor Prima de Riesgo de Trabajo<strong style="color: red">*</strong></label>
                         @if(isset($empresa))
                             <input type="number" min="-1" max="10" step="any" name="primaRiesgo" class="form-control" value="{{ $empresa->primaRiesgo }}">
                             @error('primaRiesgo')
@@ -426,7 +448,7 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>Fondo de Ahorro (%):</label>
+                        <label>Fondo de Ahorro (%)<strong style="color: red">*</strong></label>
                         @if(isset($empresa))
                             <input type="number" name="porcentajeAhorro" class="form-control porcentajeAhorro" value="{{ $empresa->porcentajeAhorro }}">
                             @error('porcentajeAhorro')
@@ -446,7 +468,7 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="form-group">
-                        <label>Regimen Fiscal:</label>
+                        <label>Regimen Fiscal<strong style="color: red">*</strong></label>
                         @if(isset($empresa))
                             <input type="text" class="form-control obtenerRegimen" value="{{ $empresa->descripcionRegimen }}">
                             <input type="hidden" name="regimenFiscal"  class="form-control idRegimen"  value="{{ $empresa->claveRegimen }}">
@@ -469,29 +491,10 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-3">
-                    <div class="form-group">
-                        <label>CURP Representante Legal:</label>
-                        @if(isset($empresa))
-                            <input type="text" name="curpRepresentante" class="form-control" value="{{ $empresa->curpRepresentante}}" >
-                            @error('curpRepresentante')
-                            <div class="alert alert-secondary">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                        @else
-                            <input type="text" name="curpRepresentante" class="form-control">
-                            @error('curpRepresentante')
-                            <div class="alert alert-secondary">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                        @endif
-                    </div>
-                </div>
+               
                 <div class="col-sm-2">
                     <div class="form-group">
-                        <label>Días del Periodo:</label>
+                        <label>Días del Periodo<strong style="color: red">*</strong></label>
                         @if(isset($empresa))
                             <input type="number" maxlength="2" name="tipoPeriodo" class="form-control tagperiodo" value="{{ $empresa->tipoPeriodo }}">
                             @error('tipoPeriodo')
@@ -511,7 +514,7 @@
                 </div>
                 <div class="col-sm-2">
                     <div class="form-group">
-                        <label>Inicio del Periodo:</label>
+                        <label>Inicio del Periodo<strong style="color: red">*</strong></label>
                         @if(isset($empresa))
                             <input type="date" name="inicioPeriodo" class="form-control" value="{{ $empresa->inicioPeriodo }}">
                             @error('inicioPeriodo')
@@ -531,7 +534,7 @@
                 </div>
                 <div class="col-sm-2" id="pagoPeriodo">
                     <div class="form-group">
-                        <label>Fecha pago periodo:</label>
+                        <label>Fecha pago periodo<strong style="color: red">*</strong></label>
                         @if(isset($empresa))
                             <input type="date" name="fechapago" class="form-control">
                             @error('inicioPeriodo')
