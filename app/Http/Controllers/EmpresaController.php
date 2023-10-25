c658692c95f70bbd131ae2f7ee4fe1b3d342bd1d
Elizabeth <anely_lyzzye@hotmail.com>
2023-10-25 16:46:51 -0600

    prueba

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -97,6 +97,7 @@ class EmpresaController extends Controller{
                 return view('empresas.crudempresas', compact('empresa','nominas'));
             break;
             case 'registrar':
+                return $request;
                 $this->registrar($request);
                 return redirect()->route('nominas.empresas');
                 break;
